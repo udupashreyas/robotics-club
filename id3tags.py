891e3ff7ffad81d88ab9fc4b1ec1d28367da397b
udupashreyas <udupa_shreyas@yahoo.co.in>
2015-01-24 20:34:38 +0530

    Delete id3tags.py

--- id3tags.py
+++
@@ -1,7 +0,0 @@
-path = "\path to mp3 file"
-from mutagen.id3 import ID3
-audio = ID3(path)
-
-print "Artist : %s" % audio["TPE1"].text[0]
-print "Track: %s" % audio["TIT2"].text[0]
-print "Release Year: %s" % audio["TDRC"].text[0]
